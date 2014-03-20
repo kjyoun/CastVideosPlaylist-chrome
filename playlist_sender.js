039b5f127c9e5e8bc051a91a8409d4a174375adc
lnicholls <lnicholls@google.com>
2014-03-19 22:39:10 -0700

    Fix app id

--- playlist_sender.js
+++ playlist_sender.js
@@ -66,7 +66,7 @@ if (!chrome.cast || !chrome.cast.isAvailable) {
  * initialization
  */
 function initializeCastApi() {
-  var applicationID = '0C32DCD2';
+  var applicationID = 'YOUR_APP_ID';
   var sessionRequest = new chrome.cast.SessionRequest(applicationID);
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
     sessionListener,
